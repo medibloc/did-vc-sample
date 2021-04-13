08c60bdc8b0fcadbc3e3c152416c1c8deb3ced82
Youngjoon Lee <yjlee@medibloc.org>
2021-04-13 23:52:11 +0900

    Simply verifier

--- verifier/src/main/java/org/medibloc/vp_verifier/Main.java
+++ verifier/src/main/java/org/medibloc/vp_verifier/Main.java
@@ -4,13 +4,11 @@ import com.rabbitmq.client.*;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.nio.charset.StandardCharsets;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.util.UUID;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.TimeoutException;
+import java.util.function.Supplier;
 
 public class Main {
     public static void main(String[] args) throws IOException, TimeoutException, URISyntaxException, NoSuchAlgorithmException, KeyManagementException, InterruptedException {
@@ -23,29 +21,24 @@ public class Main {
         try {
             Channel channel = conn.createChannel();
             try {
-                String corrId = UUID.randomUUID().toString();
-
                 String replyQueueName = channel.queueDeclare().getQueue();
-                AMQP.BasicProperties props = new AMQP.BasicProperties
-                        .Builder()
-                        .correlationId(corrId)
-                        .replyTo(replyQueueName)
-                        .build();
 
-                channel.basicPublish("", "rpc_queue", props, "I'm a client".getBytes(StandardCharsets.UTF_8));
-
-                final BlockingQueue<String> response = new ArrayBlockingQueue<>(1);
-
-                String ctag = channel.basicConsume(replyQueueName, true, ((consumerTag, message) -> {
-                    if (message.getProperties().getCorrelationId().equals(corrId)) {
-                        response.offer(new String(message.getBody(), "UTF-8"));
+                RpcClientParams rpcClientParams = new RpcClientParams();
+                rpcClientParams.exchange("");
+                rpcClientParams.routingKey("rpc_queue");
+                rpcClientParams.channel(channel);
+                rpcClientParams.replyTo(replyQueueName);
+                rpcClientParams.correlationIdSupplier(new Supplier<String>() {
+                    @Override
+                    public String get() {
+                        return UUID.randomUUID().toString();
                     }
-                }), consumerTag -> {
                 });
 
-                String resp = response.take();
-                channel.basicCancel(ctag);
-                System.out.println("response: " + resp);
+                RpcClient rpcClient = new RpcClient(rpcClientParams);
+
+                String response = rpcClient.stringCall("I'm a client");
+                System.out.println("response: " + response);
             } finally {
                 channel.close();
             }
